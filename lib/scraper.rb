546091a2c779d3875bf80fe75625fd3141f78691
Nolan Hughes <nhughes987@gmail.com>
2017-11-18 17:08:54 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,10 @@ class Scraper
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
       doc.css(".post").each do |post|
-        
+        course = Course.new
+        course.title = doc.css(".post").first.css("h2").text
+        course.schedule = doc.css(".post").first.css(".date").text
+        course.description = doc.css(".post").first.css("p").text
       end
     end
     #
@@ -30,9 +33,7 @@ class Scraper
     # end
     #
     # def make_courses
-    # title: doc.css(".post").first.css("h2").text
-    # schedule: doc.css(".post").first.css(".date").text
-    # description: doc.css(".post").first.css("p").text
+
     # end
 
 end
