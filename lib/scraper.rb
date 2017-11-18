92d989f5e0c1306b7a1f901fec31b432a57ef1f7
Nolan Hughes <nhughes987@gmail.com>
2017-11-18 17:17:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,6 @@ class Scraper
 
     def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
       doc.css(".post").each do |post|
         course = Course.new
         course.title = post.css("h2").text
@@ -38,4 +37,3 @@ class Scraper
     # end
 
 end
-
