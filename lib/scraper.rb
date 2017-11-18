647eda3beb9b1ea51c0680660b4402415eb27ebb
Nolan Hughes <nhughes987@gmail.com>
2017-11-18 17:17:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,17 +29,6 @@ class Scraper
       binding.pry
     end
 
-    # def get_page
-    #   doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    #
-    #   doc.css(".post").each do |post|
-    #     course = Course.new
-    #     course.title = doc.css(".post").first.css("h2").text
-    #     course.schedule = doc.css(".post").first.css(".date").text
-    #     course.description = doc.css(".post").first.css("p").text
-    #   end
-    #   binding.pry
-    # end
     #
     # def get_courses
     #   self.get_page.css("#course-grid")
